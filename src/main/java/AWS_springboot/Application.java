7998f08019b5d8a4a8c5ec46894a4e72263e7e76
kwonjieon <holemart@naver.com>
2024-07-10 22:59:57 +0900

    롬복테스트

---
+++ src/main/java/AWS_springboot/Application.java
@@ -0,0 +1,11 @@
+package AWS_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class,args);
+    }
+}
