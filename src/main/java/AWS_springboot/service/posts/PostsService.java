24ee35a6135999861d1a5c6bce1f2999aaf68540
kwonjieon <holemart@naver.com>
2024-07-12 20:57:42 +0900

    posts등록 api

---
+++ src/main/java/AWS_springboot/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package AWS_springboot.service.posts;
+
+import AWS_springboot.domain.posts.PostsRepository;
+import AWS_springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
