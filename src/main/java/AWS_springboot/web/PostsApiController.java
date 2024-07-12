faf108c6b9c7e55a9fdad18e9665daf9c52018ee
kwonjieon <holemart@naver.com>
2024-07-12 22:02:35 +0900

    posts수정/조회 api

--- src/main/java/AWS_springboot/web/PostsApiController.java
+++ src/main/java/AWS_springboot/web/PostsApiController.java
@@ -2,12 +2,11 @@ package AWS_springboot.web;
 
 
 import AWS_springboot.service.posts.PostsService;
+import AWS_springboot.web.dto.PostsResponseDto;
 import AWS_springboot.web.dto.PostsSaveRequestDto;
+import AWS_springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,5 +17,13 @@ public class PostsApiController {
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
 
+    }
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable("id") Long id,@RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id,requestDto);
+    }
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable("id") Long id){//이름명시
+        return postsService.findById(id);
     }
 }
