6b5d9b4c7c138714c8609b7e14e971dd84dc3c93
kwonjieon <holemart@naver.com>
2024-07-14 01:06:57 +0900

    게시글 전체조회

--- src/main/java/AWS_springboot/service/posts/PostsService.java
+++ src/main/java/AWS_springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package AWS_springboot.service.posts;
 
 import AWS_springboot.domain.posts.Posts;
 import AWS_springboot.domain.posts.PostsRepository;
+import AWS_springboot.web.dto.PostsListResponseDto;
 import AWS_springboot.web.dto.PostsResponseDto;
 import AWS_springboot.web.dto.PostsSaveRequestDto;
 import AWS_springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -30,5 +34,11 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
         return new PostsResponseDto(entity);
 
+    }
+    public List<PostsListResponseDto> findAllDesc(){
+
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)//.map(posts->new PostsListResponseDto(posts))와 같음
+                .collect(Collectors.toList());
     }
 }
